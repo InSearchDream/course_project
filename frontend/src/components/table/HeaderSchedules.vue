7882215dd4365548f400fc62c55905c5510e0e51
AlexLynx96 <nastyat1996@yandex.ru>
2022-01-05 23:24:57 +0300

    frontend

---
+++ frontend/src/components/table/HeaderSchedules.vue
@@ -0,0 +1,71 @@
+<template>
+  <div class="container">
+    <h3>Заголовки расписания</h3>
+    <div v-if="message" class="alert alert-success">{{ this.message }}</div>
+    <div class="container">
+      <table class="table">
+        <thead>
+          <tr>           
+            <th>Заголовок</th>
+			<th>Утверждено</th>
+			<th>Обновить</th>
+			<th>Удалить</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="headerschedule in headerschedules" v-bind:key="headerschedule.id_header_schedule">          
+            <td>{{ headerschedule.header_name }}</td>
+			<td>{{ headerschedule.approved }}</td>
+			<td>
+              <button class="btn" v-on:click="updateHeaderSchedule(headerschedule.id_header_schedule)">
+                Update
+              </button>
+            </td>
+            <td>
+              <button class="btn" v-on:click="deleteHeaderSchedule(headerschedule.id_header_schedule)">
+                Delete
+              </button>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+     <!-- <div class="row">
+        <button class="btn" v-on:click="addHeaderSchedule()">Добавить</button>
+      </div>-->
+    </div>
+  </div>
+</template>
+<script>
+import HeaderSchedulesDataService from "../service/DataService";
+
+export default {
+  name: "HeaderSchedules",
+  data() {
+    return {
+      headerschedules: [],
+      message: "",
+    };
+  },
+  methods: {
+    refreshHeaderSchedules() {
+      HeaderScheduleDataService.retrieveAllHeaderSchedules().then((res) => {
+        this.headerschedules = res.data;
+      });
+    },
+    addHeaderSchedule() {
+      this.$router.push(`/headerschedule/addHeaderSchedule`);
+    },
+    updateHeaderSchedule(id_header_schedule) {
+      this.$router.push(`/headerschedule/${id_header_schedule}`);
+    },
+    deleteHeaderSchedule(id_header_schedule) {
+      HeaderScheduleDataService.deleteHeaderSchedule(id_header_schedule).then(() => {
+        this.refreshHeaderSchedules();
+      });
+    },
+  },
+  created() {
+    this.refreshHeaderSchedules();
+  },
+};
+</script>
---
+++ frontend/src/service/DataService.js
@@ -0,0 +1,154 @@
+import axios from 'axios'
+
+
+const API_URL = 'http://localhost:8080/'
+
+
+class DataService {
+//Trainer
+    retrieveAllTrainers() {
+        return axios.get(`${API_URL}/trainers/getTrainers`);
+    }
+
+    retrieveTrainer(id_trainer) {
+        return axios.get(`${API_URL}/trainers/getTrainerById`);
+    }
+
+    deleteTrainer(id_trainer) {
+
+       return axios.delete(`${API_URL}/trainers/deleteTrainer`);
+    }
+
+    updateTrainer(id_trainer, trainer) {
+
+        return axios.put(`${API_URL}/trainers/updateTrainer`, trainer);
+    }
+
+    createTrainer(trainer) {
+
+        return axios.post(`${API_URL}/trainers/addTrainer`, trainer);
+    }
+	
+//Learner
+    retrieveAllLearners() {
+        return axios.get(`${API_URL}/learners/getLearners`);
+    }
+
+    retrieveLearner(id_learner) {
+        return axios.get(`${API_URL}/learners/getLearnerById`);
+    }
+
+    deleteLearner(id_learner) {
+
+       return axios.delete(`${API_URL}/learners/deleteLearner`);
+    }
+
+    updateLearner(id_learner, learner) {
+
+        return axios.put(`${API_URL}/learners/updateLearner`, learner);
+    }
+
+    createLearner(learner) {
+
+        return axios.post(`${API_URL}/learners/addLearner`, learner);
+    }
+	
+//Place
+    retrieveAllPlaces() {
+        return axios.get(`${API_URL}/places/getPlaces`);
+    }
+
+    retrievePlace(id_place) {
+        return axios.get(`${API_URL}/places/getPlaceById`);
+    }
+
+    deletePlace(id_place) {
+
+       return axios.delete(`${API_URL}/places/deletePlace`);
+    }
+
+    updatePlace(id_place, place) {
+
+        return axios.put(`${API_URL}/places/updatePlace`, place);
+    }
+
+    createPlace(place) {
+
+        return axios.post(`${API_URL}/places/addPlace`, place);
+    }
+	
+//Section
+    retrieveAllSections() {
+        return axios.get(`${API_URL}/sections/getSections`);
+    }
+
+    retrieveSection(id_section) {
+        return axios.get(`${API_URL}/sections/getSectionById`);
+    }
+
+    deleteSection(id_section) {
+
+       return axios.delete(`${API_URL}/sections/deleteSection`);
+    }
+
+    updateSection(id_section, section) {
+
+        return axios.put(`${API_URL}/sections/updateSection`, section);
+    }
+
+    createSection(section) {
+
+        return axios.post(`${API_URL}/sections/addSection`, section);
+    }
+	
+//Schedule
+    retrieveAllSchedules() {
+        return axios.get(`${API_URL}/schedules/geSchedules`);
+    }
+
+    retrieveSchedule(id_schedule) {
+        return axios.get(`${API_URL}/schedules/getScheduleById`);
+    }
+
+    deleteSchedule(id_schedule) {
+
+       return axios.delete(`${API_URL}/schedules/deleteSchedule`);
+    }
+
+    updateSchedule(id_schedule, schedule) {
+
+        return axios.put(`${API_URL}/schedules/updateSchedule`, schedule);
+    }
+
+    createSchedule(schedule) {
+
+        return axios.post(`${API_URL}/schedules/addSchedule`, schedule);
+    }
+	
+//HeaderSchedule
+    retrieveAllHeaderSchedules() {
+        return axios.get(`${API_URL}/headerschedules/getHeaderSchedules`);
+    }
+
+    retrieveHeaderSchedule(id_header_schedule) {
+        return axios.get(`${API_URL}/headerschedules/getHeaderScheduleById`);
+    }
+
+    deleteHeaderSchedule(id_header_schedule) {
+
+       return axios.delete(`${API_URL}/headerschedules/deleteHeaderSchedule`);
+    }
+
+    updateHeaderSchedule(id_header_schedule, headerschedule) {
+
+        return axios.put(`${API_URL}/headerschedules/updateHeaderSchedule`, headerschedule);
+    }
+
+    createHeaderSchedule(headerschedule) {
+
+        return axios.post(`${API_URL}/headerschedules/addHeaderSchedule`, headerschedule);
+    }
+   
+}
+
+export default new DataService()
