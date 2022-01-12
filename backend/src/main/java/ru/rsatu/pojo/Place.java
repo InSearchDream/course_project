85ca4ce9e2196e1582a2f156ab451d143d692219
AlexLynx96 <nastyat1996@yandex.ru>
2022-01-12 16:35:44 +0300

    parent class

--- backend/src/main/java/ru/rsatu/pojo/Place.java
+++ backend/src/main/java/ru/rsatu/pojo/Place.java
@@ -1,13 +1,13 @@
 package ru.rsatu.pojo;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
 @Table(name = "place")
 public class Place {
     @Id
+    @SequenceGenerator(name = "placeSeq", sequenceName = "place_id_seq", allocationSize = 1, initialValue = 1)
+    @GeneratedValue(generator = "placeSeq")
     private Long id_place;
     private String place_name;
 
