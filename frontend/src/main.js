6a486d04ba9e1bf231d54badb2e23282a04ae9a3
Kuznetsov Vadim <90772765+vekww5@users.noreply.github.com>
2022-01-14 17:42:28 +0300

    component v-select
    need install him: npm install vue-select

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,6 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './routes';
 
+//выбрать лучшее место для импорта vue-select или оставить тут
+import vSelect from 'vue-select'
+import 'vue-select/dist/vue-select.css';
+Vue.component('v-select', vSelect)
+
 Vue.config.productionTip = false
 
 new Vue({
