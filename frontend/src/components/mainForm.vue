c253fcc6eb458f6fc733caabacebd546c236fa1e
AlexLynx96 <nastyat1996@yandex.ru>
2022-01-14 13:45:31 +0300

    Update mainForm

--- frontend/src/components/mainForm.vue
+++ frontend/src/components/mainForm.vue
@@ -4,42 +4,9 @@
 	<h3>SportsTraining</h3>
     <button class="btn" v-on:click="btLogin()">Войти</button>
 
-	<!--Удалить позже, для быстрого открывания таблиц-->
-	<hr>
-	<div class="row">
-    <button class="btn" v-on:click="Section()">Секция</button>
-	<button class="btn" v-on:click="Trainer()">Тренер</button>
-	<button class="btn" v-on:click="Schedule()">Расписание</button>
-	<button class="btn" v-on:click="HS()">Заголовки</button>
-	<button class="btn" v-on:click="Place()">Места</button>
-	<button class="btn" v-on:click="Learner()">Ученики</button>
-	</div>
-  </div>
 </template>
 
 <script>
-export default {
-	methods: {
-		Section() {
-			this.$router.push(`/sections`);
-		},
-		Trainer() {
-			this.$router.push(`/trainers`);
-		},
-		Place() {
-			this.$router.push(`/places`);
-		},
-		HS() {
-			this.$router.push(`/headerschedules`);
-		},
-		Learner() {
-			this.$router.push(`/learners`);
-		},
-		Schedule() {
-			this.$router.push(`/schedules`);
-		},
-		
-	}
-}
+
 </script>
 
