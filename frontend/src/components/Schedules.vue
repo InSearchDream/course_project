dc69cf27df1b932c0a5440da7f114a640c4ddde9
Kuznetsov Vadim <90772765+vekww5@users.noreply.github.com>
2022-01-20 20:23:33 +0300

    Update Schedules.vue

--- frontend/src/components/Schedules.vue
+++ frontend/src/components/Schedules.vue
@@ -1,41 +1,39 @@
 <template>
   <div class="container">
-  <main-header/>
+    <main-header/>
     <h3>Общее расписание</h3>
-	<b-row>
-		<b-col md="3">
-			<b-form-input v-model="filter" type="search" placeholder="Найти"> </b-form-input>
-		</b-col>
-	</b-row>
-	<b-row>
-		<b-col>
-			<b-table		
-			striped
-			hover
-			:items="schedules"
-			:per-page="perPage"
-			:current-page="currentPage"
-			:filter="filter"
-			:fields="fields">
-			<template v-if="$keycloak.hasRealmRole('editSch')||$keycloak.hasRealmRole('updateSch')" v-slot:cell(Update)="data">
-				<b-button variant="btn" @click="updateSchedule(data.item[0].id_schedule)">Δ</b-button>
-			</template>
-			<template v-else v-slot:cell(Update)>Δ</template>
-			<template v-if="$keycloak.hasRealmRole('editSch')" v-slot:cell(Delete)="data">
-				<b-button variant="btn" @click="deleteSchedule(data.item[0].id_schedule)">-</b-button>
-			</template>
-			<template v-else v-slot:cell(Delete)>-</template>
-			</b-table>
-			<b-pagination
-			class="pagination"
-			v-model="currentPage"
-			:total-rows="rows"
-			:per-page="perPage"
-			></b-pagination>
-		</b-col>
-	</b-row>
-	<div v-if="$keycloak.hasRealmRole('editSch')" class="row">
-        <button class="btn" v-on:click="addSchedule()">Добавить</button>
+    <b-row>
+      <b-col md="3">
+        <b-form-input v-model="filter" type="search" placeholder="Найти"></b-form-input>
+      </b-col>
+    </b-row>
+    <b-row>
+      <b-col>
+        <b-table
+            striped
+            hover
+            :items="schedules"
+            :per-page="perPage"
+            :current-page="currentPage"
+            :filter="filter"
+            :fields="visibleFields">
+          <template v-slot:cell(Update)="data">
+            <b-button variant="btn" @click="updateSchedule(data.item[0].id_schedule)">Δ</b-button>
+          </template>
+          <template v-slot:cell(Delete)="data">
+            <b-button variant="btn" @click="deleteSchedule(data.item[0].id_schedule)">-</b-button>
+          </template>
+        </b-table>
+        <b-pagination
+            class="pagination"
+            v-model="currentPage"
+            :total-rows="rows"
+            :per-page="perPage"
+        ></b-pagination>
+      </b-col>
+    </b-row>
+    <div v-if="$keycloak.hasRealmRole('editSch')" class="row">
+      <button class="btn" v-on:click="addSchedule()">Добавить</button>
     </div>
   </div>
 </template>
@@ -50,23 +48,23 @@ export default {
   data() {
     return {
       fields: [
-		//{key: 'id_schedule', label: "ИД"}, 
-		{key:"2.header_name", label: "Заголовок"},
-		{key:"2.approved", label: ""},
-		{key: '3.section_name', label: "Секция"},
-		{key: '1.place_name', label: "Место проведения"/*, sortable: true, sortDirection: 'desc'*/ },		
-		{key:"0.date", label: "Дата"},
-		{key:"0.time_start", label: "Время начала"},
-		{key:"0.time_end", label: "Время окончания"},
-		{key:"0.cancell", label: "Отмена занятия"},
-		{key:"0.note", label: "Примечание"},
-		{key:"Update",label: "Update"},
-		{key:"Delete", label: "Delete"}],           	  
+        //{key: 'id_schedule', label: "ИД"},
+        {key: "2.header_name", label: "Заголовок", visible:true},
+        {key: "2.approved", label: "", visible:true},
+        {key: '3.section_name', label: "Секция", visible:true},
+        {key: '1.place_name', label: "Место проведения", visible:true/*, sortable: true, sortDirection: 'desc'*/},
+        {key: "0.date", label: "Дата", visible:true},
+        {key: "0.time_start", label: "Время начала", visible:true},
+        {key: "0.time_end", label: "Время окончания", visible:true},
+        {key: "0.cancell", label: "Отмена занятия", visible:true},
+        {key: "0.note", label: "Примечание", visible:true},
+        {key: "Update", label: "Update", visible:false},
+        {key: "Delete", label: "Delete", visible:false}],
       schedules: [],
       filter: "",
       message: "",
       perPage: 5,
-      currentPage: 1,	
+      currentPage: 1,
     };
   },
   methods: {
@@ -88,99 +86,17 @@ export default {
     },
   },
   computed: {
-      rows() {
-        return this.schedules.length
-      }
+    visibleFields() {
+      return this.fields.filter(field => field.visible)
+    },
+    rows() {
+      return this.schedules.length
+    }
   },
   created() {
     this.refreshSchedules();
+    this.fields[9].visible = this.$router.app.$keycloak.hasRealmRole('editSch') || this.$router.app.$keycloak.hasRealmRole('updateSch')
+    this.fields[10].visible = this.$router.app.$keycloak.hasRealmRole('editSch')
   },
 };
 </script>
-
-<!--<template>
-  <div class="container">
-  <main-header/>
-    <h3>Общее расписание</h3>
-    <div v-if="message" class="alert alert-success">{{ this.message }}</div>
-    <div class="container">
-      <table class="table">
-        <thead>
-          <tr>    
-            <th>Место проведения</th>
-			<th>Секция</th>
-			<th>Заголовок</th>	
-			<th>Дата</th>
-            <th>Время начала</th>
-            <th>Время окончания</th>
-			<th>Отмена занятия</th>
-            <th>Примечание</th>
-			<th>Обновить</th>
-			<th>Удалить</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr v-for="schedule in schedules" v-bind:key="schedule.id_schedule">          
-            <td>{{ schedule.id_place }}</td>
-			<td>{{ schedule.id_section }}</td>
-			<td>{{ schedule.id_header_schedule }}</td>
-            <td>{{ schedule.date }}</td>
-            <td>{{ schedule.time_start }}</td>
-			<td>{{ schedule.time_end }}</td>
-            <td>{{ schedule.cancell }}</td>
-            <td>{{ schedule.note }}</td>
-			<td>
-              <button class="btn" v-on:click="updateSchedule(schedule.id_schedule)">
-                Update
-              </button>
-            </td>
-            <td>
-              <button class="btn" v-on:click="deleteSchedule(schedule.id_schedule)">
-                Delete
-              </button>
-            </td>
-          </tr>
-        </tbody>
-      </table>
-      <div class="row">
-        <button class="btn" v-on:click="addSchedule()">Добавить</button>
-      </div>
-    </div>
-  </div>
-</template>
-<script>
-import ScheduleDataService from "../service/DataService";
-import MainHeader from "@/components/MainHeader";
-
-export default {
-  name: "Schedules",
-  components: {MainHeader},
-  data() {
-    return {
-      schedules: [],
-      message: "",
-    };
-  },
-  methods: {
-    refreshSchedules() {
-      ScheduleDataService.retrieveAllSchedules().then((res) => {
-        this.schedules = res.data;
-      });
-    },
-    addSchedule() {
-      this.$router.push(`/schedules/-1`);
-    },
-    updateSchedule(id_schedule) {
-      this.$router.push(`/schedules/${id_schedule}`);
-    },
-    deleteSchedule(id_schedule) {
-      ScheduleDataService.deleteSchedule(id_schedule).then(() => {
-        this.refreshSchedules();
-      });
-    },
-  },
-  created() {
-    this.refreshSchedules();
-  },
-};
-</script>-->
