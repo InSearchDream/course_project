760df9a204b928c6081eb68e3a32d110271dedab
Kuznetsov Vadim <90772765+vekww5@users.noreply.github.com>
2022-01-20 20:21:26 +0300

    Update Trainers.vue

--- frontend/src/components/Trainers.vue
+++ frontend/src/components/Trainers.vue
@@ -1,41 +1,39 @@
 <template>
   <div class="container">
-  <main-header/>
+    <main-header/>
     <h3>Данные о тренерах</h3>
-	<b-row>
-		<b-col md="3">
-			<b-form-input v-model="filter" type="search" placeholder="Найти"> </b-form-input>
-		</b-col>
-	</b-row>
-	<b-row>
-		<b-col>
-			<b-table		
-			striped
-			hover
-			:items="trainers"
-			:per-page="perPage"
-			:current-page="currentPage"
-			:filter="filter"
-			:fields="fields">
-			<template v-if="$keycloak.hasRealmRole('editTrainer')" v-slot:cell(Update)="data">
-				<b-button variant="btn" @click="updateTrainer(data.item.id_trainer)">Δ</b-button>
-			</template>
-			<template v-else v-slot:cell(Update)>Δ</template>
-			<template v-if="$keycloak.hasRealmRole('editTrainer')" v-slot:cell(Delete)="data">
-				<b-button variant="btn" @click="deleteTrainer(data.item.id_trainer)">-</b-button>
-			</template>
-			<template v-else v-slot:cell(Delete)>-</template>
-			</b-table>
-			<b-pagination
-			class="pagination"
-			v-model="currentPage"
-			:total-rows="rows"
-			:per-page="perPage"
-			></b-pagination>
-		</b-col>
-	</b-row>
-	<div v-if="$keycloak.hasRealmRole('editTrainer')" class="row" >
-        <button class="btn" v-on:click="addTrainer()">Добавить</button>
+    <b-row>
+      <b-col md="3">
+        <b-form-input v-model="filter" type="search" placeholder="Найти"></b-form-input>
+      </b-col>
+    </b-row>
+    <b-row>
+      <b-col>
+        <b-table
+            striped
+            hover
+            :items="trainers"
+            :per-page="perPage"
+            :current-page="currentPage"
+            :filter="filter"
+            :fields="visibleFields">
+          <template v-slot:cell(Update)="data">
+            <b-button variant="btn" @click="updateTrainer(data.item.id_trainer)">Δ</b-button>
+          </template>
+          <template v-slot:cell(Delete)="data">
+            <b-button variant="btn" @click="deleteTrainer(data.item.id_trainer)">-</b-button>
+          </template>
+        </b-table>
+        <b-pagination
+            class="pagination"
+            v-model="currentPage"
+            :total-rows="rows"
+            :per-page="perPage"
+        ></b-pagination>
+      </b-col>
+    </b-row>
+    <div v-if="$keycloak.hasRealmRole('editTrainer')" class="row">
+      <button class="btn" v-on:click="addTrainer()">Добавить</button>
     </div>
   </div>
 </template>
@@ -50,19 +48,19 @@ export default {
   data() {
     return {
       fields: [
-		//{key: 'id_trainer', label: "ИД"}, 
-		{key: 'last_name', label: "Фамилия"/*, sortable: true, sortDirection: 'desc'*/ },
-		{key: 'first_name', label: "Имя"}, 
-		{key:"middle_name", label: "Отчество"},
-		{key:"phone", label: "Телефон"},
-		{key:"birthday", label: "Дата рождения"},
-		{key:"Update",label: "Update"},
-		{key:"Delete", label: "Delete"}],           	  
+        //{key: 'id_trainer', label: "ИД"},
+        {key: 'last_name', label: "Фамилия", visible: true/*, sortable: true, sortDirection: 'desc'*/},
+        {key: 'first_name', label: "Имя", visible: true},
+        {key: "middle_name", label: "Отчество", visible: true},
+        {key: "phone", label: "Телефон", visible: true},
+        {key: "birthday", label: "Дата рождения", visible: true},
+        {key: "Update", label: "Update", visible: false},
+        {key: "Delete", label: "Delete", visible: false}],
       trainers: [],
       filter: "",
       message: "",
       perPage: 5,
-      currentPage: 1,	
+      currentPage: 1,
     };
   },
   methods: {
@@ -84,93 +82,17 @@ export default {
     },
   },
   computed: {
-      rows() {
-        return this.trainers.length
-      }
+    visibleFields() {
+      return this.fields.filter(field => field.visible)
+    },
+    rows() {
+      return this.trainers.length
+    }
   },
   created() {
     this.refreshTrainers();
+    this.fields[5].visible = this.$router.app.$keycloak.hasRealmRole('editTrainer')
+    this.fields[6].visible = this.$router.app.$keycloak.hasRealmRole('editTrainer')
   },
 };
 </script>
-
-<!--<template>
-  <div class="container">
-  <main-header/>
-    <h3>Данные о тренерах</h3>
-    <div v-if="message" class="alert alert-success">{{ this.message }}</div>
-    <div class="container">
-      <table class="table">
-        <thead>
-          <tr>           
-            <th>Фамилия</th>
-			<th>Имя</th>
-			<th>Отчество</th>
-            <th>Телефон</th>
-            <th>Дата рождения</th>
-			<th>Обновить</th>
-			<th>Удалить</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr v-for="trainer in trainers" v-bind:key="trainer.id_trainer">          
-            <td>{{ trainer.last_name }}</td>
-			<td>{{ trainer.first_name }}</td>
-			<td>{{ trainer.middle_name }}</td>
-            <td>{{ trainer.phone }}</td>
-            <td>{{ trainer.birthday }}</td>
-			<td>
-              <button class="btn" v-on:click="updateTrainer(trainer.id_trainer)">
-                Update
-              </button>
-            </td>
-            <td>
-              <button class="btn" v-on:click="deleteTrainer(trainer.id_trainer)">
-                Delete
-              </button>
-            </td>
-          </tr>
-        </tbody>
-      </table>
-        <div class="row">
-        <button class="btn" v-on:click="addTrainer()">Добавить</button>
-      </div>
-    </div>
-  </div>
-</template>
-<script>
-import TrainerDataService from "../service/DataService";
-import MainHeader from "@/components/MainHeader";
-
-export default {
-  name: "Trainers",
-  components: {MainHeader},
-  data() {
-    return {
-      trainers: [],
-      message: "",
-    };
-  },
-  methods: {
-    refreshTrainers() {
-      TrainerDataService.retrieveAllTrainers().then((res) => {
-        this.trainers = res.data;
-      });
-    },
-    addTrainer() {
-      this.$router.push(`/trainers/-1`);
-    },
-    updateTrainer(id_trainer) {
-      this.$router.push(`/trainers/${id_trainer}`);
-    },
-    deleteTrainer(id_trainer) {
-      TrainerDataService.deleteTrainer(id_trainer).then(() => {
-        this.refreshTrainers();
-      });
-    },
-  },
-  created() {
-    this.refreshTrainers();
-  },
-};
-</script>-->
